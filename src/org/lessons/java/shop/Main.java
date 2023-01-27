bc2990b9af29699e5a4c1aca2feaa8092b27f791
AngeloDima <Angelodimauro91@gmail.com>
2023-01-27 15:45:55 +0100

    Fine

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -3,7 +3,7 @@ package org.lessons.java.shop;
 public class Main {
 
 	public static void main(String[] args) {
-		Prodotto Kart = new Prodotto("Kart", "Kart 125cc e 250cc", 32.70);
+		Prodotto Kart = new Prodotto("Kart", "Kart 125cc e 250cc", 350);
 		System.out.println("Codice Prodotto = "+Kart.codice);
 		Kart.printBasePrice(Kart.prezzo);
 		Kart.printivaPrice(Kart.prezzo);
